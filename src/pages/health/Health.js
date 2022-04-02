2a0369c0de6c7fdf5f537b96e4767817d212e107
Nathalia Duarte <duartenatha@gmail.com>
2022-04-02 13:58:08 -0300

    feito os get, começo de estilizar

--- src/pages/health/Health.js
+++ src/pages/health/Health.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import moment from 'moment'
+
+import { GetContext } from '../../context/GetContext'
+import placeholder from "../../img/placeholder.jpg"
 
 function Health() {
+  const {getNews, news} = useContext(GetContext)
+ 
+  useEffect(() => {
+    getNews('health')
+    
+  }, [])
+
+
   return (
-    <div>Health</div>
-  )
+    <div> 
+      {news.map((e) =>(
+      <div key={e.published_date}>
+      <h3>{e.title}</h3>
+      {e.multimedia !== null ? 
+      <img src={e.multimedia[1].url} /> 
+      : <img src={placeholder} />} 
+      <p>Publication date: {moment().format('MM/DD/YYYY', e.published_date)}</p>
+      <p>Written by: {e.byline}</p>
+      </div>
+      ))}
+        </div>)
 }
+
 
 export default Health
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,36 +1,29 @@
 import React, { useContext, useEffect } from 'react'
-import { GetContext } from '../../context/GetContext'
 import moment from 'moment'
+
+import { GetContext } from '../../context/GetContext'
+import placeholder from "../../img/placeholder.jpg"
 
 function Home() {
   const {getNews, news} = useContext(GetContext)
-  let id = 0;
-  let array = []
  
- 
-
-
   useEffect(() => {
     getNews('home')
     
-    
   }, [])
-
 
   return (
     <div> 
       {news.map((e) =>(
       <div key={e.published_date}>
-          <h3>{e.title}</h3>
-          {array.push(e.multimedia)}
-          {console.log(array, "aqui deus")}
-          {/* <img src={array.find(ee=> ee.url !== undefined)} />
-          {array.shift()} */}
-          <p>Data de Publicação: {moment().format('DD/MM/YYYY', e.published_date)}</p>
-          <p>Escrito por: {e.byline}</p>
-          {/* {array = []} */}
-      </div>
-))}
+      <h3>{e.title}</h3>
+      {e.multimedia !== null ? 
+      <img src={e.multimedia[1].url} /> 
+      : <img src={placeholder} />} 
+      <p>Publication date: {moment().format('MM/DD/YYYY', e.published_date)}</p>
+      <p>Written by: {e.byline}</p>
+    </div>
+      ))}
         </div>)
 }
 
